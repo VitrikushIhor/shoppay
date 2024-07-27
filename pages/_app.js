8d7a4fbd1b455b0ffba0bafa523f6a029640e1af
Ihor <vitrikushihor0101@gmail.com>
2024-07-27 19:30:56 +0300

    try to fix next-auth 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
-import '../styles/globals.scss';
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
-import Head from 'next/head';
-import { SessionProvider } from 'next-auth/react';
-import { Provider } from 'react-redux';
-import { ToastContainer } from 'react-toastify';
-import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
+import "../styles/globals.scss";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import Head from "next/head";
+import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from '../store';
+import store from "../store";
 
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+
 let persistor = persistStore(store);
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
 		<>
